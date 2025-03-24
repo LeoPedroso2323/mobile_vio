b138aaf7d394da6592aba77c499f362e6c2d06ee
LeoPedroso2323 <leonardopedroso857@gmail.com>
2025-03-24 14:46:33 -0300

    atualização2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './screens/LoginScreen';
-import CadastroScreen from './screens/CadastroScreen';
-import HomeScreen from './screens/HomeScreen';
-import CadastroEventoScreen from './screens/CadastroEventoScreen';
-import CadastroOrganizadorScreen from './screens/CadastroOrganizadorScreen';
-import CadastroIngressoScreen from './screens/CadastroIngressoScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Layout from "./components/Layout";
+import LoginScreen from "./screens/LoginScreen";
+import CadastroScreen from "./screens/CadastroScreen";
+import HomeScreen from "./screens/HomeScreen";
+import CadastroEventoScreen from "./screens/CadastroEventoScreen";
+import CadastroOrganizadorScreen from "./screens/CadastroOrganizadorScreen";
+import CadastroIngressoScreen from "./screens/CadastroIngressoScreen";
+import Login from "./screens/LoginScreen";
+import Cadastro from "./screens/CadastroScreen";
 
 const Stack = createStackNavigator();
 
@@ -14,12 +17,32 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Cadastro" component={CadastroScreen} />
+        <Stack.Screen
+          name="Login"
+          component={() => (
+            <Layout>
+              <Login />
+            </Layout>
+          )}
+        />
+        <Stack.Screen
+          name="Cadastro"
+          component={() => (
+            <Layout>
+              <Cadastro />
+            </Layout>
+          )}
+        />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="CadastroEvento" component={CadastroEventoScreen} />
-        <Stack.Screen name="CadastroOrganizador" component={CadastroOrganizadorScreen} />
-        <Stack.Screen name="CadastroIngresso" component={CadastroIngressoScreen} />
+        <Stack.Screen
+          name="CadastroOrganizador"
+          component={CadastroOrganizadorScreen}
+        />
+        <Stack.Screen
+          name="CadastroIngresso"
+          component={CadastroIngressoScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
